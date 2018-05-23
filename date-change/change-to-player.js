593f251ac07a3c5f3976a8fe4f98eaa3f808492b
genglang <657578302@qq.com>
2018-05-24 00:07:46 +0800

    'changebysecond'

--- date-change/change-to-player.js
+++ date-change/change-to-player.js
@@ -1,4 +1,4 @@
-function ChangeToPlayer (date) {
+function ChangeByDate (date) {
 	let hour = date.getHours()
 	let min = date.getMinutes()
 	let sec = date.getSeconds()
@@ -14,4 +14,22 @@ function ChangeToPlayer (date) {
 	return `${hour} : ${min} : ${sec}`
 }
 
-console.log(ChangeToPlayer(new Date()))
+function ChangeBySecond (second) {
+	let hour = Math.floor(second / 3600)
+	let min = Math.floor((second % 3600) / 60)
+	let sec = Math.floor(second % 60)
+	if (hour < 10) {
+		hour = '0' + hour
+	}
+	if (min < 10) {
+		min = '0' + min
+	}
+	if (sec < 10) {
+		sec = '0' + sec
+	}
+	console.log(hour)
+	console.log(min)
+	console.log(sec)
+	return `${hour} : ${min} : ${sec}`
+
+}
