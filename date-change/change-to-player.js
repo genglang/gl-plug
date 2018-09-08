513e88a5ddd86ab8c86d9752dc5f444c4a456902
genglang <657578302@qq.com>
2018-09-08 23:57:06 +0800

    '修改排序算法'

--- date-change/change-to-player.js
+++ date-change/change-to-player.js
@@ -18,18 +18,8 @@ function ChangeBySecond (second) {
 	let hour = Math.floor(second / 3600)
 	let min = Math.floor((second % 3600) / 60)
 	let sec = Math.floor(second % 60)
-	if (hour < 10) {
-		hour = '0' + hour
-	}
-	if (min < 10) {
-		min = '0' + min
-	}
-	if (sec < 10) {
-		sec = '0' + sec
-	}
-	console.log(hour)
-	console.log(min)
-	console.log(sec)
+	hour = hour < 10 ? '0' + hour : hour
+	min = min < 10 ? '0' + min : min
+	sec = sec < 10 ? '0' + sec : sec
 	return `${hour} : ${min} : ${sec}`
-
 }
---
+++ sortCompare/sortByType.js
@@ -0,0 +1,10 @@
+function sortByType(sortArray) {
+	let regCn = new RegExp('^[\\u4E00-\\u9FFF]+', 'g');
+	let regEn = new RegExp('^[a-zA-Z]+', 'g');
+	let regNum = new RegExp('^[0-9]+', 'g');
+
+	let {CnArry,EnArry,NumArry}=[]
+	console.log(CnArry)
+	console.log(EnArry)
+	console.log(NumArry)
+}
