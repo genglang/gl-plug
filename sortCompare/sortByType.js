7fdadf3cc648fd3ba7b2b3ab00afe93f7c504463
genglang <657578302@qq.com>
2018-09-09 00:42:07 +0800

    '重写sortByType方法'

--- sortCompare/sortByType.js
+++ sortCompare/sortByType.js
@@ -1,10 +1,35 @@
-function sortByType(sortArray) {
-	let regCn = new RegExp('^[\\u4E00-\\u9FFF]+', 'g');
-	let regEn = new RegExp('^[a-zA-Z]+', 'g');
-	let regNum = new RegExp('^[0-9]+', 'g');
+function sortByType (sortArray) {
+	let regCn = new RegExp('^[\\u4E00-\\u9FFF]+')
+	let regEn = new RegExp('^[a-zA-Z]+')
+	let regNum = new RegExp('[0-9]+')
 
-	let {CnArry,EnArry,NumArry}=[]
-	console.log(CnArry)
-	console.log(EnArry)
-	console.log(NumArry)
+	groups = {
+		cnArry: [],
+		enArry: [],
+		numArry: [],
+		otherArry: [],
+	}
+	for (let sortItem of sortArray) {
+		if (regCn.test(sortItem)) {
+			groups.cnArry.push(sortItem)
+		} else if (regEn.test(sortItem)) {
+			groups.enArry.push(sortItem)
+		} else if (regNum.test(sortItem)) {
+			groups.numArry.push(sortItem)
+		} else {
+			groups.otherArry.push(sortItem)
+		}
+	}
+	let sortedArray = []
+	for (let groupName of Object.keys(groups)) {
+		if (groups[groupName].length > 0) {
+			if (groupName === 'numArry') {
+				groups[groupName].sort((a, b) => a - b)
+			} else {
+				groups[groupName].sort()
+			}
+			sortedArray=sortedArray.concat(groups[groupName])
+		}
+	}
+	return sortedArray
 }
